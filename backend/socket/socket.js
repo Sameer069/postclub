e568df8f4b471041e1ed906a237a8335a650a552
Das Sameer <112536688+Sameer069@users.noreply.github.com>
2025-04-14 00:53:16 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,7 +4,7 @@ const {Server} =require("socket.io")
   const setSocket=(serevr)=>{
      io=new Server(serevr,{
         cors:{
-           origin:"http://localhost:4000",
+           origin:"https://postclub-1.onrender.com",
            credentials:true
            
           }
