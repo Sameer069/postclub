371e657f160b74a5c19adcf5b84bb356b814946d
Das Sameer <112536688+Sameer069@users.noreply.github.com>
2025-04-13 22:44:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(exprees.text())
 app.use(cookieParser())
 app.use(cors(
    {
-      origin:"http://localhost:4000",
+      origin:"https://spectacular-khapse-50e6af.netlify.app/",
       credentials:true  
    }
 ))
